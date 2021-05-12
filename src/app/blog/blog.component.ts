b5a17cae458b2dc7610307d19cc63cfeccc5d3bc
Gustavo Rodriguez <gustavguez@gmail.com>
2021-05-11 22:19:17 -0300

    feat(Skully): use transfer state to prevent double request

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { TransferStateService } from '@scullyio/ng-lib';
 
 @Component({
   selector: 'app-blog',
@@ -10,12 +11,17 @@ export class BlogComponent implements OnInit {
   pokemon: any;
 
   constructor(
-    private httpClient: HttpClient ) { 
+    private httpClient: HttpClient,
+    private transferState: TransferStateService
+  ) { 
     this.pokemon = null;
   }
 
   ngOnInit(): void {
-    this.httpClient.get('https://pokeapi.co/api/v2/pokemon/1').subscribe(
+    this.transferState.useScullyTransferState(
+      'pokemons/1',
+      this.httpClient.get('https://pokeapi.co/api/v2/pokemon/1')
+    ).subscribe(
       (response: any) => {
         this.pokemon = response;
       }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { TransferStateService } from '@scullyio/ng-lib';
 
 @Component({
   selector: 'app-home',
@@ -11,12 +12,17 @@ export class HomeComponent implements OnInit {
   pokemons: any;
 
   constructor(
-    private httpClient: HttpClient ) { 
+    private httpClient: HttpClient,
+    private transferState: TransferStateService  
+  ) { 
     this.pokemons = [];
   }
 
   ngOnInit(): void {
-    this.httpClient.get('https://pokeapi.co/api/v2/pokemon').subscribe(
+    this.transferState.useScullyTransferState(
+      'pokemons',
+      this.httpClient.get('https://pokeapi.co/api/v2/pokemon')
+    ).subscribe(
       (response: any) => {
         this.pokemons = response.results;
       }
