3d5e46a15ef7af1ce4bd8c2c1d03e0adac80358a
Gustavo Rodriguez <gustavguez@gmail.com>
2021-05-20 22:59:44 -0300

    feat(General): start web using tailwindcss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { BlogComponent } from './blog/blog.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    BlogComponent,
-    NotFoundComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ScullyLibModule
+    SharedModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
